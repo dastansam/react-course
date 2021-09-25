5163fe5b1775b794cdaa2b113e918fea8cda3318
SamatovRuslan <85801346+SamatovRuslan@users.noreply.github.com>
2021-09-25 18:07:13 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import SignIn from './SignIn';
+import Routes from './Routes'
+import Forgot from "./Forgot";
 
 export default function App() {
   return (
     <div className="App">
-      <SignIn />
+      <Routes />
     </div>
   );
 }
